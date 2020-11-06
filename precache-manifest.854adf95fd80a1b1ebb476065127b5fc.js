d1bca0efd9779c9846541d36547feeffec27bacd
Guillaume <xlgktakx@gmail.com>
2020-11-06 18:45:33 +0100

    Updates

---
+++ precache-manifest.854adf95fd80a1b1ebb476065127b5fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89bf863382dc7e40ff215843672d9e6",
+    "url": "/quizzDataViz/index.html"
+  },
+  {
+    "revision": "2094d28e0dabbb89882d",
+    "url": "/quizzDataViz/static/css/main.10666ad1.chunk.css"
+  },
+  {
+    "revision": "5c135453ce171395ced9",
+    "url": "/quizzDataViz/static/js/2.3504f2fd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/quizzDataViz/static/js/2.3504f2fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2094d28e0dabbb89882d",
+    "url": "/quizzDataViz/static/js/main.1cdc7024.chunk.js"
+  },
+  {
+    "revision": "d960be705909a280144e",
+    "url": "/quizzDataViz/static/js/runtime-main.46cb8862.js"
+  }
+]);
